73da6e5473cb97220fce3f8527cbbf546d7b201b
Neochees <113420296+Neochees@users.noreply.github.com>
2023-09-01 15:50:55 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// document.getElementById('informatie').style.display = 'none';
+
+
+// var infopopup = document.getElementById('info');
+
+// infopopup.onclick = function(){
+//     var element = document.getElementById('informatie');
+//     var isVisible = element.style.display === 'none' ? false : true;
+
+//     element.style.display = isVisible ? 'none' : 'block';
+// };
+
+
+1 == '1'   // true
+1 === '1'  // false
